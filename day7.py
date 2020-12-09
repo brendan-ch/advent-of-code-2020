71708679492b60ac2b6befb9488f516a4efd4a1b
Brendan <brendann.ch@gmail.com>
2020-12-08 20:56:34 -0800

    make part 1 code way faster

--- day7.py
+++ day7.py
@@ -20,25 +20,32 @@ def constructRuleDict(rule: str):
   return ruleDict
 
 def searchForOptions(bagColor: str, ruleDicts: list):
-  """Search the rule to see if it contains bagColor. Returns list of bags that match the bagColor."""
-  matchingRuleDicts = []
+  """Search the rule to see if it contains bagColor. 
+  Returns list of bags that match the bagColor."""
+  # this function takes significantly less time to run than the previous algorithm used.
 
-  for ruleDict in ruleDicts:
-    # ruledicts contains lists of bags to search
-    listRuleDicts = [ruleDict]
+  # lambda returns boolean depending on whether rule has color
+  contains = lambda rule, color: len([bag for bag in rule['contains'] if (bag['color'] == color)]) != 0
 
-    for ruleDictToSearch in listRuleDicts:
-      for bag in ruleDictToSearch['contains']:
-        # append to matchingRuleDicts if bag is found
-        if (bag['color'] == bagColor and not ruleDict in matchingRuleDicts): 
-          matchingRuleDicts.append(ruleDict)
-          break
-        
-        # get the bag to search and store it for later
-        ruleDictToSearch = [rule for rule in ruleDicts if rule['color'] == bag['color']][0]
-        listRuleDicts.append(ruleDictToSearch)
+  # for every rule, runs lambda to see if bag color exists directly inside rule
+  listRulesWithColor = lambda color, rules: [rule for rule in rules
+    if (contains(rule, color))
+  ]
 
-  return matchingRuleDicts
+  # store rules directly (and indirectly) containing bagColor
+  ruleDictsContainingColor = listRulesWithColor(bagColor, ruleDicts)
+
+  # we need to append rules that indirectly contain bagColor to ruleDictsContainingColor
+  # to do this, we need to loop through each rule in ruleDictsContainingColor,
+  # and append rules containing that color to ruleDictsContainingColor.
+  for x in ruleDictsContainingColor:
+    # we iterate through rules that directly contain the color of x
+    for y in listRulesWithColor(x['color'], ruleDicts):
+      if (not y in ruleDictsContainingColor): 
+        ruleDictsContainingColor.append(y)  # ...and append that rule.
+      # if there are no rules, this for statement won't run
+
+  return ruleDictsContainingColor
 
 def getNumBagColors(bagColor: str, ruleDicts: list):
   """Get the number of bag colors that can contain at least one bagColor."""
